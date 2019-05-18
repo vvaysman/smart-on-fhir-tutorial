2000ff0af54039b903c7a8d8fa25cb2ad93f6b6f
vvaysman <buy@medside.com>
2019-05-17 20:22:00 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,7 +12,7 @@
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'Encounter',
+                    type: 'AllergyIntolerance',
                     query: {
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
